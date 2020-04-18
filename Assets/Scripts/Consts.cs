1c5a14c60330c044480a5f9eb24092f8bedc0304
Heftyfunseeker <niccosimone@gmail.com>
2020-04-18 10:53:00 -0700

    Shared types are now in Consts.cs. Migrated TestCritter out to separate file Critter.cs

---
+++ Assets/Scripts/Consts.cs
@@ -0,0 +1,22 @@
+
+public enum JOBK // tag = jobk
+{
+    WarmHome,
+    StoreFood,
+    CollectFood,
+    Build,
+}
+public enum RESOURCEK // tag = resk
+
+{
+    Food,
+    WarmBed,
+    Work, // time?
+}
+
+public enum NEEDK // tag = needk
+{
+    Food,
+    Warmth,
+    // lubrication?
+}
