eecbcd1b091262bbdd748b3dee72bea2f918de92
Daniel <49131731+danieldobyns@users.noreply.github.com>
2020-04-18 11:24:35 -0700

    Converted project to 2D rendering pipeline
    Converted the project to use Unity's new 2D rendering pipeline, which is optmized for 2D and even enables things like 2D lighting.
    
    Also added a few placeholder building tile sprites to get started prototyping WFC with.

--- Assets/Scripts/Buildings/BuildingModule.cs
+++ Assets/Scripts/Buildings/BuildingModule.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[CreateAssetMenu (menuName = "LD46/BuildingModule")]
+[CreateAssetMenu (menuName = "LD46/Building Module")]
 public class BuildingModule : ScriptableObject
 {
+    [Tooltip("Which set does this module belong to")]
+    public BuildingModuleSet mModuleSet = null;
+
     [Tooltip("Identifies what neighboring modules are compatible per cardinal edge")]
     public List<BuildingModule> mLeftNeightborModules;
     public List<BuildingModule> mRightNeightborModules;
     public List<BuildingModule> mUpperNeightborModules;
     public List<BuildingModule> mLowerNeightborModules;
 
+    public Sprite mSprite;
 
+    private void OnEnable()
+    {
+        mModuleSet.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        mModuleSet.Remove(this);
+    }
 }
