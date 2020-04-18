441b9ee26fff8446a488834a5ab921d15a7d4d81
Daniel <49131731+danieldobyns@users.noreply.github.com>
2020-04-18 14:15:07 -0700

    Adding Building, Slot, and Module prefabs and placeholder sprites

---
+++ Assets/Scripts/Buildings/BuildingModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LD46/Building Module")]
+public class BuildingModule : ScriptableObject
+{
+    [Tooltip("Which set does this module belong to")]
+    public BuildingModuleSet mModuleSet = null;
+
+    [Tooltip("Identifies what neighboring modules are compatible per cardinal edge")]
+    public List<BuildingModule> mLeftNeightborModules;
+    public List<BuildingModule> mRightNeightborModules;
+    public List<BuildingModule> mUpperNeightborModules;
+    public List<BuildingModule> mLowerNeightborModules;
+
+    public Sprite mSprite;
+
+    private void OnEnable()
+    {
+        if (mModuleSet)
+           mModuleSet.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        if (mModuleSet)
+            mModuleSet.Remove(this);
+    }
+}
