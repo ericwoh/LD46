a4ce0deee7b231ee57c484205917dfac00550a82
Daniel <49131731+danieldobyns@users.noreply.github.com>
2020-04-19 00:10:57 -0700

    Adding grocery sprites and cleaning up buildings code a little

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Buildings are containers with a tile-based width and height. Buildings contain a grid of 
+/// BuildingSlots, which are containers that can be filled by building BuildintModules in a slot.
+/// Building is the primary object in the building concept that interacts wiith other components
+/// </summary>
 public class Building : MonoBehaviour
 {
+    #region Properties
+    /// ------------------------------------ PUBLIC PROPERTIES --------------------------------- ///
+    
+    [Tooltip("GameObject prefab for empty BuildingSlots. DEPRECATE when this is generated at runtime")]
     public GameObject mEmptySlotPrefab;
-
 
     [Tooltip("This is the set of modules (tiles) that are used to build this building")]
     public BuildingModuleSet mModuleSet;
@@ -15,18 +23,30 @@ public class Building : MonoBehaviour
     [Tooltip("Height of the building in module units (e.g., 2 is 2 modules high)")]
     public int mBuildingHeight = 2;
 
-    public List<GameObject> mSlots;
-    private List<int> mModuleBuildOrder;
-    private float mSlotWidth = 1.0f;
-    private float mSlotHeight = 1.0f;
-
-    private int mLastModuleBuilt = 0;
-    private int mEmptySlots;
-
     [Tooltip("UI prefab to spawn on top of this object in UI space")]
     public GameObject mUiPrefab;
 
-    #region InternalMethods
+    #endregion
+
+    #region Internal Members
+    /// ------------------------------------ INTERNAL MEMBERS ---------------------------------- ///
+
+    private List<GameObject> mSlots;                    // List of BuildingSlots for this building
+    private float mSlotWidth = 1.0f;                    // Width (world space units) of a Slot
+    private float mSlotHeight = 1.0f;                   // Height (world space units) of a Slot
+
+    private int mLastModuleBuiltIndex = 0;              // index in mSlots of the last module built      
+    private int mEmptySlots = 0;                        // number of current empty slots in building
+
+    #endregion
+
+    #region Internal Unity Methods
+    
+    /// <summary>
+    /// OnEnable is a Unity method called whever the root GameObject of this script is first enabled
+    /// This can be used for startup or initialization logic, and if relevant, cleanup logic can use
+    /// OnDisable which is guaranteed to be called on object deactivate/disable or Destruction
+    /// </summary>
     private void OnEnable()
     {
         mEmptySlots = mBuildingWidth * mBuildingHeight;
@@ -38,9 +58,8 @@ public class Building : MonoBehaviour
 
         if (mSlots == null)
             mSlots = new List<GameObject>(mBuildingWidth * mBuildingHeight);
-        mModuleBuildOrder = new List<int>(mSlots.Count);
 
-        // iterate through, instantiating slots floot by floor moving upward, e.g. visually:
+        // iterate through, instantiating slots floot by floor moving upward, e.g. list indeices visually:
         // 8 9
         // 4 5 6 7
         // 0 1 2 3
@@ -53,7 +72,19 @@ public class Building : MonoBehaviour
                 slotPosition.y += i * mSlotHeight + (mSlotHeight * 0.5f);
 
                 // instantiate a BuildingSlot object at this position as a child of this object
-                mSlots.Add(Instantiate(mEmptySlotPrefab, slotPosition, Quaternion.identity, transform));
+                //mSlots.Add(Instantiate(mEmptySlotPrefab, slotPosition, Quaternion.identity, transform));
+
+                GameObject emptySlotObject = new GameObject("emptySlot" + (i + j));
+                emptySlotObject.transform.parent = transform;
+                emptySlotObject.transform.position = slotPosition;
+                emptySlotObject.AddComponent<SpriteRenderer>();
+                SpriteRenderer sr = emptySlotObject.GetComponent<SpriteRenderer>();
+                sr.sprite = null;
+                sr.sortingLayerName = "BuildingSlots";
+                emptySlotObject.AddComponent<BuildingSlot>();
+
+                mSlots.Add(emptySlotObject);
+
             }
         }
 
@@ -61,12 +92,13 @@ public class Building : MonoBehaviour
         // start each building with a front door module
         int doorSlot = (int)(((mBuildingWidth * 0.5f) - 1));
         mSlots[doorSlot].GetComponent<BuildingSlot>().SetBuildingModule(mModuleSet.mDoorModule);
-        mLastModuleBuilt = doorSlot;
+        mLastModuleBuiltIndex = doorSlot;
         mEmptySlots -= 1;
     }
     #endregion
 
     #region Public Methods
+    /// ------------------------------------- PUBLIC METHODS ----------------------------------- ///
 
     /// <summary>
     /// Public method calls to have this building build a new module if not already full
@@ -87,6 +119,20 @@ public class Building : MonoBehaviour
         mEmptySlots -= 1;
     }
 
+    public void ClearBuildingModules()
+    {
+        foreach (GameObject go in mSlots)
+        {
+            BuildingSlot slot = go.GetComponent<BuildingSlot>();
+            slot.ClearSlot();
+        }
+    }
+
+    #endregion
+
+    #region Internal Helper Methods
+    /// ------------------------------------ INTERNAL METHODS ---------------------------------- ///
+
     /// <summary>
     /// Randomly select a slot to build in as long as it's not empty below
     /// </summary>
@@ -97,12 +143,15 @@ public class Building : MonoBehaviour
             Debug.LogError("Attempted to build module in building that's already full.");
 
         bool foundNextSlot = false;
-        int nextbuildSlot = mLastModuleBuilt;
+        int nextbuildSlot = mLastModuleBuiltIndex;
 
-        float timeStarted = Time.time;
+        int iteration = 0;
         while (!foundNextSlot)
         {
-            if (mSlots[nextbuildSlot].GetComponent<BuildingSlot>().IsSlotEmpty())
+            nextbuildSlot = Random.Range(0, mSlots.Count);
+
+            BuildingSlot nextSlot = mSlots[nextbuildSlot].GetComponent<BuildingSlot>();
+            if (nextSlot.IsSlotEmpty())
             {
                 if (nextbuildSlot < mBuildingWidth)
                     return nextbuildSlot;   // ground floor, good to build.
@@ -114,9 +163,9 @@ public class Building : MonoBehaviour
             }                
 
             // watchdog timer
-            if (Time.time - timeStarted > 3.0f)
+            if (++iteration > 30)
             {
-                Debug.LogError("Took over 3 seconds to find an empty slot, something is broken.");
+                Debug.LogError("Took over 30 iterations to find an empty slot, something is broken.");
                 return -1;
             }    
         }
--- Assets/Scripts/Buildings/BuildingSlot.cs
+++ Assets/Scripts/Buildings/BuildingSlot.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Building slot represents a tile space within a building that a BuildingModule can be built
+/// It also handles showing the Sprite for the current BuildingModule in this slot.
+/// </summary>
 public class BuildingSlot : MonoBehaviour
 {
     private BuildingModule mModule = null;
@@ -20,5 +24,16 @@ public class BuildingSlot : MonoBehaviour
     public bool IsSlotEmpty()
     {
         return (mModule == null) ? true : false;
+    }
+
+    /// <summary>
+    /// Public method used to reset this building slot, reinitializing to blank state
+    /// </summary>
+    public void ClearSlot()
+    {
+        mModule = null;
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr != null)
+            sr.sprite = null;
     }
 }
