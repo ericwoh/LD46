8c6e23229a6b67a2c1e8cfa8103abd2e0a962a0a
Daniel <49131731+danieldobyns@users.noreply.github.com>
2020-04-19 00:11:25 -0700

    Actually adding new Sprites and learning Sublime Merge

---
+++ Assets/Scripts/Buildings/Editor/BuildingEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Building))]
+public class BuildingEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GUI.enabled = Application.isPlaying;
+
+        EditorGUILayout.Space();
+
+        Building b = target as Building;
+        if (GUILayout.Button("Build New Module"))
+            b.BuildNewModule();
+
+        if (GUILayout.Button("Clear Building"))
+            b.ClearBuildingModules();
+    }
+}
