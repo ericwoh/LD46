f66a4104b68ed9b95f020e982dccb7cf517f1b57
Daniel <49131731+danieldobyns@users.noreply.github.com>
2020-04-18 20:12:54 -0700

    Added mockup background ref

---
+++ Assets/Scripts/ScriptableObjectUtilities/BuildingTypesSet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Simple data container class for grouping sets of BuildingModules like "tilesets" for generating
+/// WFC-generated buildings
+/// </summary>
+[CreateAssetMenu(menuName = "LD46/Building Types Set")]
+public class BuildingTypesSet : RuntimeSet<Building>
+{
+    [Tooltip("Door module is special since all buildings should start with a door")]
+    public BuildingModuleSet mModuleSet;
+}
