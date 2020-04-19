3423842cf1f232adb5d689c8796e4e16935e85a8
Daniel <49131731+danieldobyns@users.noreply.github.com>
2020-04-19 10:26:29 -0700

    Fixed GroceryManagerSettings Unity issue by moving to its own file
    Moved GroceryManagerSettings scriptable object to its own file so Unity recognizes it as an asset data type now.

---
+++ Assets/Scripts/GroceryManagerSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LD46/Grocery Manager Settings")]
+public class GroceryManagerSettings : ScriptableObject
+{
+    public List<GameObject> _lPrefabGrocery;
+    public int _cShelf = 3;
+    public int _cSlotPerShelf = 20;
+
+    public float _tAddGroceries = 60.0f;
+    public float _tRemoveGroceries = 15.0f;
+};
